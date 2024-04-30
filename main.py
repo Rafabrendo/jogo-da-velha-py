eb66e44bf31bd9ba231cffd2c2ee72a290e67fbf
Rafael Brendo <rafalive122@hotmail.com>
2024-04-30 16:45:11 -0300

    Import grid and game board

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import pygame
 
 #pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+WINDOW_WIDTH = 720
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_WIDTH))
 clock = pygame.time.Clock()
 running = True
+
+def load_icon(path, resolution):
+    icon = pygame.image.load(path)
+    return pygame.transform.scale(icon, resolution)
+
+board = [
+    [None, None, None],
+    [None, None, None],
+    [None, None, None]
+]
+
+ICON_X = load_icon('icons/x-icon.png', [WINDOW_WIDTH // 3, WINDOW_WIDTH // 3])
+ICON_O = load_icon('icons/o-icon.png', [WINDOW_WIDTH // 3, WINDOW_WIDTH // 3])
+GRID = load_icon('icons/grid.png', [WINDOW_WIDTH, WINDOW_WIDTH])
 
 while running:
     # poll for events
@@ -14,13 +29,17 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("white")
 
     #RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    #Render your game
 
+    screen.blit(GRID, (0, 0))
+    screen.blit(ICON_X, (0, 0))
+    screen.blit(ICON_O, (WINDOW_WIDTH // 3, WINDOW_WIDTH // 3))
+    pygame.display.flip()
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
